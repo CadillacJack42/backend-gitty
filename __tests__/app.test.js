ca4bcf53d75cef4c5863bef7b50ddd2b188ce6c0
James Keicher <jamesdkeicher@gmail.com>
2022-04-01 11:29:41 -0700

    test: Should redirect users to dashboard after login

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const agent = request(app);
 
 jest.mock('../lib/utils/github');
 
@@ -15,10 +16,25 @@ describe('backend-gitty routes', () => {
   });
 
   it('Should redirect to gihub Oauth page for login', async () => {
-    const req = await request(app).get('/api/v1/github/login');
+    const res = await request(app).get('/api/v1/github/login');
 
-    expect(req.header.location).toMatch(
+    expect(res.header.location).toMatch(
       /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
     );
+  });
+
+  it('Should redirect users to dashboard after login', async () => {
+    const res = await agent
+      .get('/api/v1/github/login/callback?code=42')
+      .redirects(1);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      username: 'fake_github_user',
+      email: 'not-real@example.com',
+      avatar: expect.any(String),
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
   });
 });
